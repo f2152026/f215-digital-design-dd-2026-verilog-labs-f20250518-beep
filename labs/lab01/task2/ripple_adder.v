7e8c5f59ab7abaf5cd78a77b819fded9afff1363
Maanil Parikh <f20250518@pilani.bits-pilani.ac.in>
2026-08-29 16:28:42 +0000

    lab01 task2

--- labs/lab01/task2/ripple_adder.v
+++ labs/lab01/task2/ripple_adder.v
@@ -22,6 +22,7 @@ module ripple_adder(
 
   wire c1, c2, c3;
 
+  // TODO: your four FA_Gate instances go here.
   FA_Gate FA0 (
     .a (a[0]),
     .b (b[0]),
@@ -53,6 +54,5 @@ module ripple_adder(
     .sum (sum[3]),
     .cout (cout)
   );
-  // TODO: your four FA_Gate instances go here.
-
+  
 endmodule
